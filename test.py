c9f20ad5f12d26ffef625c2e1b215309a8bbfeaf
mrEnst-python <evgeny.stepanov@gmail.com>
2020-01-29 18:06:55 +0300

    библиотека чтения файлов XLSX

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print('Hello, World')
+import openpyxl as xlrd
+
+rb = xlrd.load_workbook('./file.xlsx')
+sheet = rb
+.get_active_sheet
+for rownum in range(sheet.nrows):
+    row = sheet.row_values(rownum)
+for c_el in row:
+    print(c_el)
